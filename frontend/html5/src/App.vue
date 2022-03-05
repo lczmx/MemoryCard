29aac5a4ccada5b1194c4546e8cebfe604a37ff4
lczmx <lczmx1739@gmail.com>
2022-03-05 16:42:33 +0800

    perf: 动态设置后端ip

--- frontend/html5/src/App.vue
+++ frontend/html5/src/App.vue
@@ -1,3 +1,32 @@
 <template>
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+export default defineComponent({
+  name: "App",
+  setup() {
+    // 动态设置后端host
+    /*
+    2.window.location.protocol（协议）
+            结果：http
+    4.window.location.hostname（域名）
+
+    */
+    const protocol = window.location.protocol;
+    const hostname = window.location.hostname;
+
+    const store = useStore();
+    store.commit(
+      "setServiceHost",
+      `${protocol}//${hostname}`
+    );
+    console.log("App ")
+    return {
+      // 返回的数据
+    };
+  },
+});
+</script>
--- frontend/html5/src/store/index.ts
+++ frontend/html5/src/store/index.ts
@@ -4,7 +4,8 @@ import persistedState from "vuex-persistedstate"; // 持久化
 export default createStore({
   state: {
     // sever hostname
-    serverHost: "http://192.168.0.110:8000",
+    serverHost: "",
+    serverPort: "8366",
     // 用于记录当前页面是否被修改
     changeState: false,
     // 是否被提交
@@ -33,6 +34,10 @@ export default createStore({
     setToken(state, { accessToken, tokenType }) {
       state.token.accessToken = accessToken;
       state.token.tokenType = tokenType;
+    },
+    setServiceHost(state, host) {
+      // 动态设置serverHost
+      state.serverHost = `${host}:${state.serverPort}`;
     },
   },
   actions: {},
